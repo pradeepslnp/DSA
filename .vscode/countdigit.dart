cec6687132b1eb2d16d1c9fc1f5f71402b80140e
pradeepslnp7 <pradeep@ezrlabs.com>
2025-01-24 11:24:38 +0530

    added

--- .vscode/countdigit.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  int a = 12345625;
-  countDigit(int val) {
-    val.abs();
-    string store = val.toString().length;
-    return print(store);
-  }
-}
--- .vscode/lib/main.dart
+++ .vscode/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 void main() {
   // reverse method
 
@@ -21,7 +23,7 @@ void main() {
   }
 
   palindrome() {
-    int orinilaNum = 123321;
+    int orinilaNum = 123001000321;
     int dupPalinNum = orinilaNum;
 
     int reverse = 0;
@@ -35,9 +37,179 @@ void main() {
     } else {
       return print(false);
     }
-    print(reverse);
   }
 
+  String convertNumberToWords(int number) {
+    if (number == 0) return "zero";
+
+    final List<String> units = [
+      "",
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine"
+    ];
+    final List<String> teens = [
+      "ten",
+      "eleven",
+      "twelve",
+      "thirteen",
+      "fourteen",
+      "fifteen",
+      "sixteen",
+      "seventeen",
+      "eighteen",
+      "nineteen"
+    ];
+    final List<String> tens = [
+      "",
+      "",
+      "twenty",
+      "thirty",
+      "forty",
+      "fifty",
+      "sixty",
+      "seventy",
+      "eighty",
+      "ninety"
+    ];
+
+    String result = "";
+
+    if (number >= 1000) {
+      int thousand = number ~/ 1000;
+      result += units[thousand] + " thousand" + " ";
+      number %= 1000;
+    }
+    if (number >= 100) {
+      int hun = number ~/ 100;
+      result += units[hun] + " hundred" + " ";
+      number %= 100;
+    }
+    if (number >= 10) {
+      int uni = number ~/ 10;
+      result += tens[uni] + " ";
+      number %= 10;
+    }
+    if (number > 0) {
+      result += units[number] + " ";
+    }
+
+    return result.trim();
+  }
+
+  int number = 3252;
+  armStrongNum(int originalNum) {
+    int dup = originalNum;
+    int digits = originalNum.toString().length;
+    int sum = 0;
+
+    while (originalNum > 0) {
+      int lastNum = originalNum % 10;
+      sum += pow(lastNum, digits).toInt();
+      originalNum = originalNum ~/ 10;
+    }
+    return sum == dup;
+  }
+
+  allDivisors(int num) {
+    int newNum = 0;
+    for (int i = 1; i <= num; i++) {
+      if (num % i == 0) {
+        newNum += i;
+      }
+    }
+    return newNum;
+  }
+
+  primeNumbers(int primNum) {
+    int count = 0;
+    for (int i = 1; i <= primNum; i++) {
+      if (primNum % i == 0) {
+        count++;
+      }
+    }
+    if (count == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  oneToN(int i) {
+    if (i < 1) {
+      return;
+    }
+    oneToN(i - 1);
+    print(i);
+  }
+
+  functionalRecursion(int n) {
+    if (n == 1) {
+      // print(n);
+      return 1;
+    }
+    return n * functionalRecursion(n - 1);
+  }
+
+  recursionPalindrome(int n) {
+    int originalNum = n;
+    int revNum = 0;
+    if (n > 0) {
+      int lastNum = n % 10;
+      revNum = (revNum * 10) + lastNum;
+      n = n ~/ 10;
+    }
+    if (n == revNum) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // reverseArray() {
+  List<T> fun<T>(List<T> abc) {
+    if (abc.length == 0) {
+      return abc;
+    }
+
+    return [abc.last] + fun(abc.sublist(0, abc.length - 1));
+  }
+
+  List list = [1, 2, 3, 4, 5, 6, 7];
+
+  print(fun(list));
+  // List reverse = [];
+  // for (int i = abc.length; i >= 0; i--) {
+  //   reverse.add(i);
+  // }
+
+  // int start = 0;
+  // int end = abc.length - 1;
+  // while (start < end) {
+  //   int temp = abc[start];
+  //   abc[start] = abc[end];
+  //   abc[end] = temp;
+  //   start++;
+  //   end--;
+  // }
+  // print(reverse);
+  // }
+
+  // reverseArray();
+  // print(recursionPalindrome(121));
+  // dynamic num = functionalRecursion(3);
+  // print(num);
+  // oneToN(4);
+  // print(primeNumbers(12));
+  // print(allDivisors(12));
+  // print(armStrongNum(143));
+  // print(convertNumberToWords(number)); // Output: one hundred
   // palindrome();
   // countDigit(a);
   // reverseMethods();
